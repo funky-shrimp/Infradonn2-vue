88d0b7a412a13b77421ce7e81dea6afd6daccbe8
Funky Shrimp <thibaud.nuss@gmail.com>
2024-10-10 08:44:05 +0200

    initial commit with DB connection

---
+++ src/views/PouchDBView.vue
@@ -0,0 +1,44 @@
+<script lang="ts">
+
+export default {
+  // Properties returned from data() become reactive state
+  // and will be exposed on `this`.
+  data() {
+    return {
+      pouchDB: null
+    }
+  },
+
+  // Methods are functions that mutate state and trigger updates.
+  // They can be bound as event handlers in templates.
+  methods: {
+    initDatabase() {
+      this.pouchDB = $pouchDB.name
+    }
+  },
+
+  // Lifecycle hooks are called at different stages
+  // of a component's lifecycle.
+  // This function will be called when the component is mounted.
+  mounted() {
+    this.initDatabase()
+  }
+}
+</script>
+
+<template>
+  <div class="couchDB">
+    <h1>CouchDB</h1>
+    <p>{{ pouchDB }}</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .couchDB {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
